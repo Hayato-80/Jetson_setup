cc4130d92e902e09f53fb77f5557d499d1dbc2bb
--global <jojohaya.fs.sb421@outlook.jp>
2023-09-19 09:33:17 +0900

    added file for setting jetson nano tools

---
+++ Jetson_nano_tools.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt-get update
+
+# Install python tool
+sudo apt-get -y install python3-pip
+sudo apt install python3-dev
+
+# Install jetson-stats
+sudo pip3 install -U jetson-stats
+
+# set swap file (6GB)
+git clone https://github.com/JetsonHacksNano/installSwapfile
+cd installSwapfile
+sudo ./installSwapfile.sh
+
+# setup for PWM fan
+git clone https://github.com/Pyrestone/jetson-fan-ctl.git
+cd jetson-fan-ctl
+sudo ./install.sh
+
+sudo apt update
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Jetson_setup
+Installing Jtop, python tools, swap file, and setting fot the pwm fan
+Execute the following commands:
+```
+cd ~/Jetson_setup
+./Jetson_nano_tools.sh
+```
+## Reference
+* https://qiita.com/karaage0703/items/b14c249aa33112669ee4

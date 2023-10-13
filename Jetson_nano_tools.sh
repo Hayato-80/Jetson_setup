bba74188510544de75713999e74d5ec94516ac3c
--global <jojohaya.fs.sb421@outlook.jp>
2023-10-13 16:12:15 +0200

    added disable zram

--- Jetson_nano_tools.sh
+++ Jetson_nano_tools.sh
@@ -16,6 +16,7 @@ sudo pip3 install -U jetson-stats
 git clone https://github.com/JetsonHacksNano/installSwapfile
 cd installSwapfile
 sudo ./installSwapfile.sh
+sudo systemctl disable nvzramconfig
 
 # setup for PWM fan
 git clone https://github.com/Pyrestone/jetson-fan-ctl.git
